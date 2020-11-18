a255eef9887b1cfc0975e5882918a6244a5d6e94
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-18 22:30:24 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,21 +1,23 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title
-
-GENRES = []
-
+  attr_reader :title, :genre
+ 
+  GENRES = []
+ 
   def initialize(title)
     @title = title
+    
   end
-
+ 
+def genre=(genre)
+  @genre = genre
+  GENRES << genre
+end
+ 
+ 
+ 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
-  def genre=(genre)
-    @genre = genre
-    GENRES << genre
-  end
-  
  
 end
